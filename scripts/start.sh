50ad0a177fab74d25825f296a8b85f571df66603
Håvard Pedersen <fuzzy76@fuzzy76.net>
2017-12-18 18:17:11 +0100

    Added SSH key generation

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,11 +4,18 @@
 
 # Check for git command
 command -v git >/dev/null 2>&1 || { echo "git missing. Make sure it is available before running." >&2; exit 1; };
+
+# Check for key, generate if missing
+if [ ! -d "$HOME/.ssh" ]; then
+  ssh-keygen -t rsa -b 4096
+fi
+
 # If we dont have the dotfiles repo, get it
 if [ ! -d "$HOME/repos/dotfiles" ]; then
   mkdir -p $HOME/repos
   git clone git@github.com:fuzzy76/dotfiles.git $HOME/repos/dotfiles
 fi
+
 # Change to dotfiles folder and start installation
 cd $HOME/repos/dotfiles || exit;
 source scripts/install.sh
